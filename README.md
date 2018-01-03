5c58c7f35506a824d25c3025ade4c413b79f6de7
Nicholas Tan <nicholas.tan@groundlabs.com>
2018-01-03 14:21:05 +0800

    Additional details and fixes

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# amkcbot
-Telegram Bot for AMK Court
+# Unofficial Ang Mo Kio Court BTO Telegram Bot
+
+
+## Description
+This is an unofficial telegram bot that is deployed in a telegram chat group for Ang Mo Kio Court BTO. 
+
+The motivation behind this bot is to aid in the easy retrieval of useful information regarding or about Ang Mo Kio Court and its neighbourhood
+### List of available commands
+
+* !cd/!countdown
+* !help/!list/!commands
+* !pdf/!brochure
+* !progress
+* !currentstate/!cs
+* !current227a/!227a
+* !current228a/!228a
+* !current228b/!228b
+* !neighbourhood/!neighborhood/!amenities
+* !toilettiles/!kitchentiles/!serviceyardtiles
+* !rules
+* !pinterest
+* !fb/!facebook"
+
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var before="completion!"
 var current="Today is the day!"
 var montharray=new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
 var finalString = ""
+
 function countdown(yr,m,d){
     theyear=yr;themonth=m;theday=d
     var today=new Date()
@@ -23,6 +24,15 @@ function countdown(yr,m,d){
     finalString = "Only "+dday+ " days left until "+before
 }
 
+function calculateProgress() {
+    var today = new Date();
+    var start = Date.parse("Nov 25 2016");
+    var end = Date.parse("Aug 31 2020");
+    var currentProgress = today - start;
+    var total = end - start;
+    var percentage = Math.round((currentProgress/total * 100)) / 100
+    finalString = percentage + "%";
+}
 
 var express = require('express');
 var app = express();
@@ -37,62 +47,92 @@ app.use(bodyParser.urlencoded({
 //This is the route the API will call
 app.post('/new-message', function(req, res) {
   const {message} = req.body
-  var input = message.text.toLowerCase()
-  //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
-  switch(input) {
-    case '!cd':
-    case '!countdown':
-      countdown(2020,8,31)
-      break;
-    case '!help':
-    case '!list':
-    case '!commands':
-      finalString = "• !cd/!countdown\n" +
-      "• !help/!list/!commands\n" +
-      "• !pdf/!brochure\n" +
-      "• !neighbourhood/!neighborhood/!amenities\n" +
-      "• !toilettiles/!kitchentiles/!serviceyardtiles\n" +
-      "• !rules\n" +
-      "• !pinterest\n" +
-      "• !fb/!facebook"
-      break;
-    case '!pdf':
-    case '!brochure':
-      finalString = "Here you go: http://esales.hdb.gov.sg/hdbvsf/eap001.nsf/0/16MAYBTO_pdf_selection_1705/$FILE/Ang_Mo_Kio_Court.pdf"
-      break;
-    case '!neighbourhood':
-    case '!neighborhood':
-    case '!amenities':
-      finalString = "• Nearest Convenience Store: SPC at 793 Ang Mo Kio Avenue 1\n" +
-      "• Nearest Petrol Station: SPC at 793 Ang Mo Kio Avenue 1\n" +
-      "• Nearest ATM: UOB/POSB at 226C Ang Mo Kio Ave 1\n" +
-      "• Nearest AXS Machine: 128 Ang Mo Kio Ave 3\n" +
-      "• Nearest Gym: Anytime Fitness at Ang Mo Kio Central\n" +
-      "• Nearest Police Post: Kebun Baru Neighborhood Police Post\n" +
-      "• Nearest Clinic: S Y Ng Clinic at 226A Ang Mo Kio Ave 1\n" +
-      "• Nearest Hospital: Mount Alvernia Hospital" 
-      break;
-    case '!toilettiles':
-    case '!kitchentiles':
-    case '!serviceyardtiles':
-      finalString = "Here you go: https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/22365621_167407407173715_1589342577267964230_n.jpg?oh=bc3febbc5019a6ba66a0e1cff66b6236&oe=5ABCE118"
-      break;
-    case '!rules':
-      finalString = "DO: \n✅ Update on new stuffs \n✅ Be active \n\nDON’T: \n❌ Strictly NO advertising allowed \n❌ Admins reserve the right to remove all advertisements and the person who posted it immediately ‼️"
-      break;
-    case '!pinterest':
-      finalString = "Here you go: https://www.pinterest.com/cchong0783/home-interior-referencing-information-corner/"
-      break;
-    case '!fb':
-    case '!facebook':
-      finalString = "Here you go: https://www.facebook.com/groups/1228159647218536/"
-      break;
-    default:
-      return res.end();
+  if (typeof message.text === "undefined") {
+    res.end();
   }
+  else {
+      var input = message.text.toLowerCase()
+      //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
+      switch(input) {
+        case '!cd':
+        case '!countdown':
+          countdown(2020,8,31)
+          break;
+        case '!help':
+        case '!list':
+        case '!commands':
+          finalString = "• !cd/!countdown\n" +
+          "• !help/!list/!commands\n" +
+          "• !pdf/!brochure\n" +
+          "• !progress\n" +
+          "• !currentstate/!cs\n" +
+          "• !current227a/!227a\n" +
+          "• !current228a/!228a\n" +
+          "• !current228b/!228b\n" +
+          "• !neighbourhood/!neighborhood/!amenities\n" +
+          "• !toilettiles/!kitchentiles/!serviceyardtiles\n" +
+          "• !rules\n" +
+          "• !pinterest\n" +
+          "• !fb/!facebook"
+          break;
+        case '!pdf':
+        case '!brochure':
+          finalString = "Here you go: https://drive.google.com/file/d/19uUbFudxvmpiWCMy8Qgx_RwjPG6nMEoQ/view"
+          break;
+        case '!progress':
+          calculateProgress();
+          break;
+        case '!currentstate':
+        case '!cs':  
+          finalString = "227A: \n http://www20.hdb.gov.sg/fi10/fi10996p.nsf/ResponsiveMarkup/AMKN2C31/$file/AMKN2C31_BLK227A.JPEG \n\n" +
+              "228A: \n http://www20.hdb.gov.sg/fi10/fi10996p.nsf/ResponsiveMarkup/AMKN2C31/$file/AMKN2C31_BLK228A.JPEG \n\n"+
+              "228B: \n http://www20.hdb.gov.sg/fi10/fi10996p.nsf/ResponsiveMarkup/AMKN2C31/$file/AMKN2C31_BLK228B.JPG"
+          break;
+        case '!current227a':
+        case '!227a':
+          finalString = "http://www20.hdb.gov.sg/fi10/fi10996p.nsf/ResponsiveMarkup/AMKN2C31/$file/AMKN2C31_BLK227A.JPEG"
+          break;
+        case '!current228a':
+        case '!228a':
+          finalString = "http://www20.hdb.gov.sg/fi10/fi10996p.nsf/ResponsiveMarkup/AMKN2C31/$file/AMKN2C31_BLK228A.JPEG"
+          break;
+        case '!current228b':
+        case '!228b':
+          finalString = "http://www20.hdb.gov.sg/fi10/fi10996p.nsf/ResponsiveMarkup/AMKN2C31/$file/AMKN2C31_BLK228B.JPG"
+          break;
+        case '!neighbourhood':
+        case '!neighborhood':
+        case '!amenities':
+          finalString = "• Nearest Convenience Store: SPC at 793 Ang Mo Kio Avenue 1\n" +
+          "• Nearest Petrol Station: SPC at 793 Ang Mo Kio Avenue 1\n" +
+          "• Nearest ATM: UOB/POSB at 226C Ang Mo Kio Ave 1\n" +
+          "• Nearest AXS Machine: 128 Ang Mo Kio Ave 3\n" +
+          "• Nearest Gym: Anytime Fitness at Ang Mo Kio Central\n" +
+          "• Nearest Police Post: Kebun Baru Neighborhood Police Post\n" +
+          "• Nearest Clinic: S Y Ng Clinic at 226A Ang Mo Kio Ave 1\n" +
+          "• Nearest Hospital: Mount Alvernia Hospital" 
+          break;
+        case '!toilettiles':
+        case '!kitchentiles':
+        case '!serviceyardtiles':
+          finalString = "Here you go: https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/22365621_167407407173715_1589342577267964230_n.jpg?oh=bc3febbc5019a6ba66a0e1cff66b6236&oe=5ABCE118"
+          break;
+        case '!rules':
+          finalString = "DO: \n✅ Update on new stuffs \n✅ Be active \n\nDON’T: \n❌ Strictly NO advertising allowed \n❌ Admins reserve the right to remove all advertisements and the person who posted it immediately ‼️"
+          break;
+        case '!pinterest':
+          finalString = "Here you go: https://www.pinterest.com/cchong0783/home-interior-referencing-information-corner/"
+          break;
+        case '!fb':
+        case '!facebook':
+          finalString = "Here you go: https://www.facebook.com/groups/1228159647218536/"
+          break;
+        default:
+          return res.end();
+      }
 
-
-  axios.post('https://api.telegram.org/bot500219655:AAFAeFdB9Ip-BE_wzYp88AjST7aXqM2gblo/sendMessage', {
+  }
+  axios.post('https://api.telegram.org/bot500219655:AAEvMocrvGn0olV-hunS_DCE-Vwppvc6Ehc/sendMessage', {
     chat_id: message.chat.id,
     text: finalString
   })
